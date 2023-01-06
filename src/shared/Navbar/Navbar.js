2d9a9fc99d63f353e6a4c66558f7a1f5d4babf70
mithon42 <mithonphilip@gmail.com>
2023-01-06 23:13:00 +0600

    rename images

--- src/shared/Navbar/Navbar.js
+++ src/shared/Navbar/Navbar.js
@@ -2,9 +2,11 @@ import { useTheme } from "@mui/material/styles";
 import { useContext, useState } from "react";
 import { ColorModeContext } from "../../theme";
 import NavbarView from "./NavbarView";
+
+
 const Navbar = () =>{
-    const colorMode = useContext(ColorModeContext);
-    const [anchorElNav, setAnchorElNav] = useState(false);
+  const colorMode = useContext(ColorModeContext);
+  const [anchorElNav, setAnchorElNav] = useState(false);
   const [anchorElUser, setAnchorElUser] = useState(null);
   const [seeProfile, setSeeProfile] =useState(false);
 
@@ -31,11 +33,11 @@ const Navbar = () =>{
 
 
 
-return (
+  return (
     <>
-    <NavbarView colorMode={colorMode} theme={theme} handleProfileMenu={handleProfileMenu} seeProfile={seeProfile} setSeeProfile={setSeeProfile} />
+      <NavbarView colorMode={colorMode} theme={theme} handleProfileMenu={handleProfileMenu} seeProfile={seeProfile} setSeeProfile={setSeeProfile} />
     </>
-)    
+  )    
 }
 
 export default Navbar
